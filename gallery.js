de2a977174cb6fbf695eb482f588a894557512bb
Tomás Salvatore <tomisalvatore@gmail.com>
2021-06-26 16:49:34 -0300

    Initial commit for the dogs gallery

---
+++ gallery.js
@@ -0,0 +1,26 @@
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+const galleryImgs = document.querySelectorAll('.gallery-img');
+let currentlySelected = 0;
+
+prevButton.addEventListener('click', function () {
+  galleryImgs[currentlySelected].classList.remove('active');
+  currentlySelected--;
+  galleryImgs[currentlySelected].classList.add('active');
+  nextButton.disabled = false;
+
+  if (currentlySelected === 0) {
+    prevButton.disabled = true;
+  }
+});
+
+nextButton.addEventListener('click', function () {
+  galleryImgs[currentlySelected].classList.remove('active');
+  currentlySelected++;
+  galleryImgs[currentlySelected].classList.add('active');
+  prevButton.disabled = false;
+
+  if (galleryImgs.length === currentlySelected + 1) {
+    nextButton.disabled = true;
+  }
+});
